ed43243cfb0ec27af7a816c01e0ad1bb3423a1e5
Timothy <sky.multigp@gmail.com>
2019-02-27 19:05:18 -0700

    fixed server to work for both python 2 and 3

--- scripts/FSNObjects.py
+++ scripts/FSNObjects.py
@@ -31,6 +31,7 @@ class Message:
         self.messageType = messageType
         self.extra = extra
 
+    @staticmethod
     def getMessage(message):
         Message.senderID = message[SENDER_ID_KEY]
         Message.messageType = message[MESSAGE_TYPE_KEY]
@@ -42,19 +43,20 @@ class Message:
         message[MESSAGE_TYPE_KEY] = self.messageType
         message[MESSAGE_EXTRA_KEY] = self.extra
         return str(message)
-    
+
 class ServerEvent:
     #server event types
     PLAYER_JOINED = 0
     PLAYER_LEFT = 1
     SERVER_QUIT = 2
     ACK = 3
-    
+
     def __init__(self, eventType, extra=None):
         self.messageType = SERVER_EVENT_TYPE_KEY
         self.eventType = eventType
         self.extra = extra
 
+    @staticmethod
     def getMessage(message):
         obj = ServerEvent(message[SERVER_EVENT_TYPE_KEY], message[MESSAGE_EXTRA_KEY])
         return obj
@@ -74,6 +76,7 @@ class ServerState(Message):
         self.playerStates = playerStates
         self.extra = extra
 
+    @staticmethod
     def getMessage(message):
         obj = ServerState(message[ServerState.PLAYER_STATES_KEY],message[MESSAGE_EXTRA_KEY])
         return obj
@@ -98,6 +101,7 @@ class PlayerState(Message):
         self.orientation = orientation
         self.color = color
 
+    @staticmethod
     def getMessage(message):
         obj = PlayerState(message[SENDER_ID_KEY],message[MESSAGE_EXTRA_KEY],message[PlayerState.PLAYER_POSITION_KEY],message[PlayerState.PLAYER_ORIENTATION_KEY],message[PlayerState.PLAYER_COLOR_KEY])
         return obj
@@ -111,18 +115,19 @@ class PlayerState(Message):
         message[self.PLAYER_ORIENTATION_KEY] = self.orientation
         message[self.PLAYER_COLOR_KEY] = self.color
         return str(message)
-    
+
 class PlayerEvent:
     #player event types
     PLAYER_JOINED = 0
     PLAYER_QUIT = 1
-    
+
     def __init__(self, eventType, senderID, extra=None):
         self.messageType = PLAYER_EVENT
         self.eventType = eventType
         self.senderID = senderID
         self.extra = extra
 
+    @staticmethod
     def getMessage(message):
         obj = PlayerEvent(message[PLAYER_EVENT_TYPE_KEY], message[SENDER_ID_KEY], message[MESSAGE_EXTRA_KEY])
         return obj
@@ -133,13 +138,4 @@ class PlayerEvent:
         message[SENDER_ID_KEY] = self.senderID
         message[PLAYER_EVENT_TYPE_KEY] = self.eventType
         message[MESSAGE_EXTRA_KEY] = self.extra
-        return str(message)    
-
-        
-
-
-
-
-
-    
-    
+        return str(message)
